e102f4979ef0e2a93796aa7aee775dc9d998478e
Asprengers <amy.sprengers@hva.nl>
2020-05-14 12:57:34 +0200

    scripts

--- opdracht1/demo/js/script.js
+++ opdracht1/demo/js/script.js
@@ -4,10 +4,32 @@
   2. Koppel een evenListener aan het element om een mouse-event te detecteren
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
-document.getElementById("detail").addEventListener("click", showDetails);
+document.getElementById("detailOne").addEventListener("click", showDetailsOne);
 
-function showDetails() {
-    var detaillist = document.getElementById('fullDetails');
+function showDetailsOne() {
+    var detaillist = document.getElementById('fullDetailsOne');
+    if (detaillist.style.display === "none") {
+        detaillist.style.display = "block";
+    } else {
+        detaillist.style.display = "none";
+    }
+}
+
+document.getElementById("detailTwo").addEventListener("click", showDetailsTwo);
+
+function showDetailsTwo() {
+    var detaillist = document.getElementById('fullDetailsTwo');
+    if (detaillist.style.display === "none") {
+        detaillist.style.display = "block";
+    } else {
+        detaillist.style.display = "none";
+    }
+}
+
+document.getElementById("detailThree").addEventListener("click", showDetailsThree);
+
+function showDetailsThree() {
+    var detaillist = document.getElementById('fullDetailsThree');
     if (detaillist.style.display === "none") {
         detaillist.style.display = "block";
     } else {
